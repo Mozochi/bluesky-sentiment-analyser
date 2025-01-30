b1516827cb998641c04fe133c103874d5253a824
Mozochi <50766817+Mozochi@users.noreply.github.com>
2025-01-30 18:05:03 +0000

    Update to __main__.py

--- __main__.py
+++ __main__.py
@@ -1,14 +1,20 @@
 import pandas as pd
+from nltk.corpus import twitter_samples
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.probability import FreqDist
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.classify import NaiveBayesClassifier
 from bin import preprocessing
 
-df = pd.read_csv('https://raw.githubusercontent.com/pycaret/pycaret/master/datasets/amazon.csv')
-df = df.drop(columns=['Positive'])
-df['Opinion'] = ''
+
+tweets = twitter_samples.strings('positive_tweets.json') + twitter_samples.strings('negative_tweets.json')
+
+df = pd.DataFrame(tweets, columns=['Text'])
+df['Sentiment'] = ''
+#df = df.drop(columns=['Positive'])
+#df['Opinion'] = ''
 analyzer = SentimentIntensityAnalyzer()
 
 # Function to determine the sentiment of a string
@@ -20,6 +26,7 @@ def sentiment(tokens):
 
 
 # Calling the Preprocessing function and the sentiment function to determine the sentiment of each review
-for row, text in enumerate(df['reviewText']):
-    df.at[row, 'Opinion'] = sentiment(preprocessing.Process(text))
+for row, text in enumerate(df['Text']):
+    df.at[row, 'Sentiment'] = sentiment(preprocessing.Process(text))
 
+df.to_csv(r'tweets.csv', index=False)
