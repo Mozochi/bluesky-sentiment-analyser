793de01e708370c4c6b36db2afac8ea8217478c2
Josh-Exton <145766031+Josh-Exton@users.noreply.github.com>
2025-02-13 19:26:25 +0000

    slighly improved gui

--- GUI.py
+++ GUI.py
@@ -10,12 +10,16 @@ class GUI:
         self.topLabel = Label(self.window, text='Facebook sentiment analyser')
         self.buttonText = 'Please enter a key word'
         self.buttonLabel = Label(self.window, text=self.buttonText)
-        self.button = Button(self.window, text='submit', width=25)
-        self.var = IntVar()
-        self.radioButton1 = Radiobutton(self.window, text='keyword input', variable=self.var, value=0)
-        self.radioButton2 = Radiobutton(self.window, text='profile input', variable=self.var, value=1)
+        self.button = Button(self.window, text='submit', width=25, command=self.submit)
+        self.radioVar = IntVar()
+        self.radioButton1 = Radiobutton(self.window, text='keyword input', variable=self.radioVar, value=0, command=self.changeButtonText)
+        self.radioButton2 = Radiobutton(self.window, text='profile input', variable=self.radioVar, value=1, command=self.changeButtonText)
         self.progressBar = ttk.Progressbar(self.window, orient="horizontal", length=300, mode="determinate")
-        self.enter = Entry(self.window)
+        self.enterVar=StringVar()
+        self.enter = Entry(self.window, textvariable = self.enterVar)
+        self.display()
+
+    def display(self):
         self.topLabel.grid(row=0, column=0)
         self.buttonLabel.grid(row=2, column=0)
         self.enter.grid(row=2, column=1)
@@ -23,6 +27,15 @@ class GUI:
         self.progressBar.grid(row=4, column=0)
         self.radioButton1.grid(row=1, column=0)
         self.radioButton2.grid(row=1, column=1)
+
+    def changeButtonText(self):
+        if (self.radioVar.get() == 0):
+            self.buttonText = 'Please enter a key word'
+
+        else:
+            self.buttonText = 'Please enter a profile name'
+
+        self.buttonLabel.config(text=self.buttonText)
 
     def startProgress(self):
         self.progressBar.start()
@@ -43,13 +56,7 @@ class GUI:
         return
 
     def submit(self):
+        enter = self.enterVar.get()
         self.startProgress()
         self.getData()
         self.analyse()
-
-    def changeButtonText(self):
-        if (self.var.get() == 0):
-            self.buttonText = 'Please enter a key word'
-
-        else:
-            self.buttonText = 'Please enter a profile name'
