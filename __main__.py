0d27d16ab60499c19904b26df4e80bd02cec79b4
Mozochi <50766817+Mozochi@users.noreply.github.com>
2025-02-25 23:14:02 +0000

    Updated main.py
    Added Comments to code

--- __main__.py
+++ __main__.py
@@ -4,7 +4,7 @@ from bin import sentiment_analyser
 
 
 def main():
-
+    # Temporary data for testing
     train_texts = [
         "I love pizza",                 # Class 1 (Positive)
         "I enjoy coding",               # Class 1 (Positive)
@@ -14,17 +14,17 @@ def main():
         "I love machine learning",      # Class 1 (Positive)
         "I hate sitting in traffic"     # Class 0 (Negative)
         ]
-    y_train = [1, 1, 0, 0, 0, 1, 0]
+    y_train = [1, 1, 0, 0, 0, 1, 0] # Labels for the training data (1 = Positive, 0 = Negative) for each text in train_texts
 
-    nb = naive_bayes.NaiveBayes()
+    nb = naive_bayes.NaiveBayes() # Creating a Naive Bayes model
     
-    model, vocab = sentiment_analyser.train(nb, train_texts, y_train)
+    model, vocab = sentiment_analyser.train(nb, train_texts, y_train) # Training the model
 
-    test_data =  ["I love coding", "I hate pizza",]
-    predictions = sentiment_analyser.predict(model, test_data, vocab)
+    test_data =  ["I love coding", "I hate pizza",] # Testing data
+    predictions = sentiment_analyser.predict(model, test_data, vocab) # Making predictions
 
-    for i, prediction in enumerate(predictions):
-        print(f"Prediction for '{test_data[i]}': {'Positive' if prediction == 1 else 'Negative'}")
+    for i, prediction in enumerate(predictions): # Printing the predictions
+        print(f"Prediction for '{test_data[i]}': {'Positive' if prediction == 1 else 'Negative'}") 
 
 
 
