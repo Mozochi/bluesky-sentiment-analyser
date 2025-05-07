8f37e605a1ca20e2304bf19f51228e48451a31aa
Josh-Exton <145766031+Josh-Exton@users.noreply.github.com>
2025-05-07 15:18:30 +0100

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,6 +1,20 @@
 import GUI
+from BSkyAPI import get_posts_from_search, get_posts_from_handle
+from analyse import model as sentiment_model_class
 
 app = GUI.GUI()
 
 
 app.launch()
+
+class controler:
+
+    def get_api_requirements(self):
+        app.predict_sentiment()
+
+    def call_api(self):
+        self.get_api_requirements()
+
+    def call_sentiment_anayliser(self):
+
+    def call_display_result(self):
